919323d6e0055b967c2bb8075fed45872fe93804
mostlyaman <amansharma110603@gmail.com>
2023-12-03 03:25:34 +0530

    create user reference to clerk

--- frontend/src/pages/api/webhook.ts
+++ frontend/src/pages/api/webhook.ts
@@ -1,15 +1,21 @@
+import { type WebhookEvent } from '@clerk/nextjs/server'
 import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import { db } from '~/server/db'
 type ResponseData = {
   message: string
 }
  
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   if(req.method === 'POST') {
-    res.status(200).json({ message: JSON.stringify(req.body) })
+    const event = req.body as WebhookEvent
+    switch(event.type) {
+      case 'email.created':
+        await db.user.create({ data: { id: event.data.id } })
+    }
+    res.status(200).json({ message: `User created: ${event.data.id}` })
   } else {
     res.status(405).json({ message: 'Method Not Allowed.' })
   }
