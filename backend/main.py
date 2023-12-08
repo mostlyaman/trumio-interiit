2263e5be0c3d748114b11e1f92c71cdcc6fef7b4
Aman Sikarwar <amansik.1910@gmail.com>
2023-12-08 23:39:42 +0530

    add repo describer

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,20 @@ from fastapi import FastAPI
 from api.models import (
     AllCommitsSummaryRequest,
     CommitDiffRequest,
+    RepoFileRequest,
+    RepoFilesDescriptionRequest,
     TranscriptRequest,
     MilestonesRequest,
     Project,
     CommitRequest,
 )
+from services.file_describe_service import describe_files
 from services.transcript_service import (
     create_prompt,
     get_chapters_from_transcription,
 )
 from services.milestone_service import generate_milestones, create_milestone_prompt
-from services.github_service import GithubService
+from services.github_service import GithubService, RepoContentRequest
 from services.commit_summary_service import summarise_commit
 import dotenv
 import os
@@ -42,6 +45,7 @@ async def get_transcript_chapters(transcript: TranscriptRequest, project: Projec
 
     return await get_chapters_from_transcription(transcript.transcript, prompt)
 
+
 @app.post("/milestones")
 async def get_milestones(project: MilestonesRequest):
     prompt = create_milestone_prompt(project)
@@ -51,14 +55,14 @@ async def get_milestones(project: MilestonesRequest):
     return await generate_milestones(prompt)
 
 
-@app.get("/commits")
+@app.post("/commits")
 async def get_commits(request: CommitRequest):
     github_service = GithubService(token)
 
     return github_service.get_commit_hashes(request)
 
 
-@app.get("/commit-diff")
+@app.post("/commit-diff")
 async def get_commit_diff(request: CommitDiffRequest):
     github_service = GithubService(token)
 
@@ -103,3 +107,39 @@ async def get_all_commits_summary(request: AllCommitsSummaryRequest):
         summaries.append(summarise_commit(commit_diff.commit_diff))
 
     return summaries
+
+
+@app.post("/repo-files")
+async def get_repo_content(request: RepoContentRequest):
+    github_service = GithubService(token)
+
+    return github_service.get_repo_files(request)
+
+
+@app.post("/repo-files-content")
+async def get_repo_files_content(request: RepoContentRequest):
+    github_service = GithubService(token)
+
+    repo_content = github_service.get_repo_files(request)
+
+    if repo_content is None:
+        return None
+
+    files = []
+
+    for file in repo_content.repo_content:
+        content = github_service.get_repo_file_content(
+            RepoFileRequest(owner=request.owner, repo=request.repo, path=file.path)
+        )
+
+        if content is None:
+            return None
+
+        files.append(content)
+
+    return files
+
+
+@app.post("/describe-repo")
+async def describe_repo(request: RepoFilesDescriptionRequest):
+    return describe_files(request)
--- backend/services/github_service.py
+++ backend/services/github_service.py
@@ -1,4 +1,6 @@
 import requests
+import utils.consts as consts
+from base64 import b64decode
 from typing import Optional
 from api.models import (
     CommitDiff,
@@ -6,6 +8,11 @@ from api.models import (
     CommitDiffResponse,
     CommitRequest,
     CommitsResponse,
+    RepoContentRequest,
+    RepoContentResponse,
+    RepoContent,
+    RepoFile,
+    RepoFileRequest,
 )
 
 
@@ -37,5 +44,78 @@ class GithubService:
 
         if response.status_code == 200:
             return CommitsResponse(hashes=[commit["sha"] for commit in response.json()])
+        else:
+            return None
+
+    def get_directory_content(self, directory: dict) -> list[RepoContent]:
+        url = directory["url"]
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            files = [
+                content
+                for content in response.json()
+                if content["type"] == "file"
+                and content["name"].endswith(tuple(consts.included_extensions))
+            ]
+
+            for directory in [
+                content for content in response.json() if content["type"] == "dir"
+            ]:
+                files.extend(self.get_directory_content(directory))
+
+            return files
+        else:
+            return []
+
+    def get_repo_files(
+        self, request: RepoContentRequest
+    ) -> Optional[RepoContentResponse]:
+        url = f"https://api.github.com/repos/{request.owner}/{request.repo}/contents"
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            files = [
+                content
+                for content in response.json()
+                if content["type"] == "file"
+                and content["name"].endswith(tuple(consts.included_extensions))
+            ]
+
+            for directory in [
+                content for content in response.json() if content["type"] == "dir"
+            ]:
+                files.extend(self.get_directory_content(directory))
+
+            return RepoContentResponse(
+                repo_content=[
+                    RepoContent(
+                        name=file["name"],
+                        path=file["path"],
+                        url=file["url"],
+                        type=file["type"],
+                    )
+                    for file in files
+                ]
+            )
+
+        else:
+            return None
+
+    def get_repo_file_content(self, request: RepoFileRequest) -> Optional[RepoFile]:
+        url = f"https://api.github.com/repos/{request.owner}/{request.repo}/contents/{request.path}"
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return RepoFile(
+                name=response.json()["name"],
+                path=response.json()["path"],
+                url=response.json()["url"],
+                type=response.json()["type"],
+                content=b64decode(response.json()["content"]).decode(
+                    "utf-8", "replace"
+                ),
+            )
+
         else:
             return None
