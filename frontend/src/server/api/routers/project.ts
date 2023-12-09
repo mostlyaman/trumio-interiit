f53ae08d6084dce54e6ce4637ddbc1e768632453
Luv Sharma <124135876+Stormbreakerr20@users.noreply.github.com>
2023-12-09 16:13:55 +0530

    Add context to master AI

--- frontend/src/server/api/routers/project.ts
+++ frontend/src/server/api/routers/project.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 import {
   createTRPCRouter,
@@ -36,33 +37,75 @@ export const projectRouter = createTRPCRouter({
         where: {
           team_members: {
             some: {
-              id: userId
-            }
-          }
+              id: userId,
+            },
+          },
+        },
+        include: {
+          github_repos: true
         }
-      })
+      });
     }),
 
   createBid: privateProcedure
     .input(
-      z
-        .object({
-          userID: z.string(),
-          bid_data: z.object({
-            milestones: z.array(MilestoneSchema),
-            start_date: z.date(),
-          }),
-        })
+      z.object({
+        userID: z.string(),
+        bid_data: z.object({
+          milestones: z.array(MilestoneSchema),
+          start_date: z.date(),
+        }),
+      }),
     )
     .mutation(({ ctx: { userId, db }, input }) => {
       const bidData = input.bid_data;
       try {
-        console.log(bidData)
-        return {}
+        console.log(bidData);
+        return {};
       } catch (error) {
-
-        console.error('Error creating/updating bid:', error);
-        throw new Error('Failed to create/update bid');
+        console.error("Error creating/updating bid:", error);
+        throw new Error("Failed to create/update bid");
       }
+    }),
+
+  addGithubRepo: privateProcedure
+    .input(
+      z.object({
+        repo: z.string().optional(),
+        username: z.string().optional(),
+        projectId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx: { db }, input }) => {
+      const username = input.username;
+      const repo = input.repo;
+
+        // const res = await fetch();
+
+      if (username && repo) {
+        const github_repo = await db.githubRepos.findFirst({
+          where: {
+            projectId: input.projectId,
+            username, repo
+          },
+          select:{
+            username:true
+          }
+        });
+        if(!github_repo) {
+          await db.githubRepos.create({
+            data : {
+              projectId: input.projectId,
+              username,
+              repo,
+            }
+          });
+        } else {
+          throw new TRPCError({code: "BAD_REQUEST" ,message: "This repository is already linked."})
+        }
+      } else {
+        throw new TRPCError({code: "BAD_REQUEST", message: "Github URL seems invalid."})
+      }
+      
     }),
 });
